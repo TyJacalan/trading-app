7b495efb8671f74d7c06c690c5fb0a79b1fc2711
Ty Jac <muzuuu@protonmail.com>
2024-03-29 17:32:02 +0800

    chore: update user model
    - add devise confirmable module
    - add role to users and set default to standard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+  enum role: { standard: 0, admin: 1 }
 
   validates :first_name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :last_name, length: { minimum: 2, maximum: 30 }, allow_blank: true
@@ -22,15 +21,6 @@ class User < ApplicationRecord
     end
     unless password.match?(/[!@#$%^&*]/)
       errors.add(:password, 'must contain at least one special character')
-    end
-  end
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.full_name = auth.info.name
-      user.avatar_url = auth.info.image
     end
   end
 end
