0ef4e260e9f6625a418a172669ba6b95ee811158
Ty Jac <muzuuu@protonmail.com>
2024-03-31 00:11:48 +0800

    chore: create transactions model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_29_091530) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_30_150914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "symbol", null: false
+    t.integer "transaction_type", null: false
+    t.decimal "price", precision: 15, scale: 8, null: false
+    t.decimal "amount", precision: 15, scale: 8, null: false
+    t.string "currency", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", default: "", null: false
@@ -35,4 +47,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_29_091530) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "transactions", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,46 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user = User.find(1)
+
+Transaction.create!(
+  symbol: 'AAPL',
+  transaction_type: 0,
+  price: 150.25,
+  amount: 100,
+  currency: 'USD',
+  user: user
+)
+
+Transaction.create!(
+  symbol: 'AAPL',
+  transaction_type: 0,
+  price: 150.25,
+  amount: 200,
+  currency: 'USD',
+  user: user
+)
+
+Transaction.create!(
+  symbol: 'AAPL',
+  transaction_type: 1,
+  price: 154.25,
+  amount: 50,
+  currency: 'USD',
+  user: user
+)
+
+Transaction.create!(
+  symbol: 'GOOGL',
+  transaction_type: 1,
+  price: 2750.50,
+  amount: 50,
+  currency: 'USD',
+  user: user
+)
+
+Transaction.create!(
+  symbol: 'MSFT',
+  transaction_type: 0,
+  price: 300.75,
+  amount: 75,
+  currency: 'USD',
+  user: user
+)
