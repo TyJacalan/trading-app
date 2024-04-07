7b97a082b7ec327d5df03c3b691464bfe71dea13
Ty Jac <muzuuu@protonmail.com>
2024-04-07 19:07:48 +0800

    feat: add image caching and increase stock list limit

--- app/controllers/concerns/stocks_concern.rb
+++ app/controllers/concerns/stocks_concern.rb
@@ -10,6 +10,7 @@ module StocksConcern
         { symbol:,
           company_name: stock.company_name,
           price: stock.latest_price,
+          logo_url: cache_image(symbol),
           change: stock.change_percent_s }
       end
     end
@@ -17,7 +18,7 @@ module StocksConcern
     def chart_stock(symbol)
       {
         symbol:,
-        logo_url: @client.logo(symbol)&.url,
+        logo_url: cache_image(symbol),
         company: @client.company(symbol),
         quote: @client.quote(symbol),
         chart_data: format_chart_data(@client.chart(symbol, '1y', chart_close_only: true)),
@@ -27,6 +28,12 @@ module StocksConcern
 
     def format_chart_data(chart_data)
       chart_data.map { |data| [data.date, data.close] }
+    end
+
+    def cache_image(symbol)
+      Rails.cache.fetch("#{symbol}_image", expires_in: 24.hours) do
+        @client.logo(symbol)&.url
+      end
     end
   end
 end
--- app/controllers/stocks_tables_controller.rb
+++ app/controllers/stocks_tables_controller.rb
@@ -2,11 +2,11 @@ class StocksTablesController < StocksController
   def show
     stock_list = case params[:filter] 
                  when 'gainers'
-                   @client.stock_market_list(:gainers)
+                   @client.stock_market_list(:gainers, listLimit: 25)
                  when 'losers'
-                   @client.stock_market_list(:losers)
+                   @client.stock_market_list(:losers, listLimit: 25)
                  else
-                   @client.stock_market_list(:mostactive)
+                   @client.stock_market_list(:mostactive, listLimit: 25)
                  end
     @stocks = map_stock_list(stock_list)
     @name = params[:filter].gsub("-", " ").titleize
