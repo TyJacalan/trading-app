979f4247e2783c1aa1c6ead79d0ff1dd58e3054b
Ty Jac <muzuuu@protonmail.com>
2024-04-08 13:22:08 +0800

    feat: add display all and display per transaction functionality

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -1,8 +1,10 @@
 class Admin::TransactionsController < AdminsController
   def index
-    @transactions = Transaction.all
+    @transactions = Transaction.order(created_at: :desc)
   end
 
   def show
+    @transaction = Transaction.find(params[:id])
+    @user = User.find(@transaction.user_id)
   end
 end
