64670b0c1074b10c3e2f5f0d18201d89e30ba60c
maeci <cimafrancamae@gmail.com>
2024-04-09 11:36:26 +0800

    feature: add shadcn-ui combobox

--- app/javascript/controllers/ui/popover_controller.js
+++ app/javascript/controllers/ui/popover_controller.js
@@ -2,7 +2,7 @@
 
 import { Controller } from "@hotwired/stimulus";
 import { createPopper } from "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js";
-import { useClickOutside } from "stimulus-use";
+import { useClickOutside } from "https://ga.jspm.io/npm:stimulus-use@0.51.3/dist/index.js";
 
 export default class UIPopover extends Controller {
   static values = {
