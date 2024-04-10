09abaa6f7f7489d68c9b57e5ae9ff2961a33d528
Ty Jac <muzuuu@protonmail.com>
2024-04-10 23:04:49 +0800

    feat: create navbar component

--- app/helpers/components/avatar_helper.rb
+++ app/helpers/components/avatar_helper.rb
@@ -3,6 +3,6 @@ module Components::AvatarHelper
     avatar_classes = "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full"
     avatar_classes << " #{options[:class]}"
     avatar_classes = tw(avatar_classes)
-    render "components/ui/avatar", avatar_classes:, src:, data:, **options
+    render "components/ui/avatar", avatar_classes: avatar_classes,  src:, data:, **options
   end
 end
