f6d6cb9d970ca776b4ac4265103d12f9bc3bc832
Ty Jac <muzuuu@protonmail.com>
2024-04-19 18:07:00 +0800

    feat: render stock information in transactions form

--- app/controllers/stocks_details_controller.rb
+++ app/controllers/stocks_details_controller.rb
@@ -2,10 +2,14 @@ class StocksDetailsController < StocksController
   def show
     @stock_chart = format_chart_data(@client.chart(params[:symbol], '1y', chart_close_only: true))
     @stock = fetch_stock(params[:symbol])
+    @transaction = Transaction.new(transaction_type: params[:transaction_type])
 
-    render turbo_stream: 
+    render turbo_stream: [
       turbo_stream.update('details', 
                           partial: 'stocks_details/info', 
-                          locals: { details: @stock, chart_data: @stock_chart })
+                          locals: { details: @stock, chart_data: @stock_chart }),
+      turbo_stream.update('transactions_form',
+                          partial: 'transactions/form',
+                          locals: { stock: @stock })]
   end
 end
