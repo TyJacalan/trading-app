4aebe78a7531182fc87b10c9f466037800b59f85
Ty Jac <muzuuu@protonmail.com>
2024-04-20 11:56:43 +0800

    chore: render suggestion items based on types form or link

--- app/controllers/stocks_search_controller.rb
+++ app/controllers/stocks_search_controller.rb
@@ -1,13 +1,14 @@
 class StocksSearchController < StocksController
   def show
     query = params[:query].present? ? params[:query].downcase : '*'
+    type = params[:as]
 
     @stocks = cache_all_stocks
     @suggestions = @stocks.select { |stock| stock[:symbol].downcase.include?(query) }
 
-    render turbo_stream: 
+    render turbo_stream:
       turbo_stream.update('suggestions', 
-                          partial: 'stocks_search/suggestions', 
-                          locals: { suggestions: @suggestions })
+                          partial: 'stocks_search/suggestions',
+                          locals: { type: type })
   end
 end
