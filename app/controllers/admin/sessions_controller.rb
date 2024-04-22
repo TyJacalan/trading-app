0e8f6f8644691552880c64f572875d2a788cb9b6
Ty Jac <muzuuu@protonmail.com>
2024-04-22 21:12:33 +0800

    fix: check if current_user exists before checking for user role
    when a user signs in with a unregistered email, an undefined role error
    will appear because the edited session controllers checks for the
    current user's role before checking if a current user exists.

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Admin::SessionsController < Devise::SessionsController
   def create
-    if current_user.role != 'admin'
+    if current_user && current_user.role && current_user.role != 'admin'
       sign_out current_user
       redirect_to new_user_session_path
     else
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,8 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   def create
-    logger.debug current_user.role
-    if current_user.role == 'admin'
+    if current_user && current_user.role && current_user.role == 'admin'
       sign_out current_user
       redirect_to admin_sign_in_path
     else
