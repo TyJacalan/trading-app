49ababdda10a4d0fa262cd6eb9e280316cd45596
Ty Jac <muzuuu@protonmail.com>
2024-04-23 19:41:13 +0800

    refactor: add value column to the transactions table and default values to quantity and price average in the stocks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_115951) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_23_103036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "stocks", force: :cascade do |t|
     t.string "symbol"
-    t.float "price_avg"
-    t.integer "quantity"
+    t.float "price_avg", default: 0.0
+    t.integer "quantity", default: 0
     t.string "currency"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_115951) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "value", precision: 15, scale: 8
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
@@ -42,7 +43,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_115951) do
     t.string "last_name", default: ""
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "full_name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -54,7 +54,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_115951) do
     t.string "unconfirmed_email"
     t.integer "role", default: 0
     t.integer "approved"
-    t.decimal "balance"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
