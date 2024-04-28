c9adba13776da8697f917cc188c5b7f3b7dda835
Ty Jac <muzuuu@protonmail.com>
2024-04-28 20:18:02 +0800

    feat: add sample data and run on production build

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,41 @@
-user = User.find(1)
-
-Transaction.create!(
-  symbol: 'AAPL',
-  transaction_type: 0,
-  price: 150.25,
-  amount: 100,
-  currency: 'USD',
-  user: user
+# Create admin user
+admin = User.create!(
+  first_name:"Admin",
+  last_name: "User",
+  email: "admin@example.com",
+  password: "password123!",
+  role: "admin",
+  confirmed_at: Time.now.utc,
+  approved: 1
 )
+admin.skip_confirmation!
+admin.save!
 
-Transaction.create!(
-  symbol: 'AAPL',
-  transaction_type: 0,
-  price: 150.25,
-  amount: 200,
-  currency: 'USD',
-  user: user
-)
+# Create sample users
+50.times do |i|
+  user = User.new(
+    first_name: "Example#{i}",
+    last_name: "User",
+    email: "example#{i}@example.com",
+    password: "password123!",
+    created_at: rand(Date.new(2024, 1, 1)..Date.new(2024,7,31)),
+    approved: 1
+  )
+  user.skip_confirmation!
+  user.save
+end
 
-Transaction.create!(
-  symbol: 'AAPL',
-  transaction_type: 1,
-  price: 154.25,
-  amount: 50,
-  currency: 'USD',
-  user: user
-)
-
-Transaction.create!(
-  symbol: 'GOOGL',
-  transaction_type: 1,
-  price: 2750.50,
-  amount: 50,
-  currency: 'USD',
-  user: user
-)
-
-Transaction.create!(
-  symbol: 'MSFT',
-  transaction_type: 0,
-  price: 300.75,
-  amount: 75,
-  currency: 'USD',
-  user: user
-)
+# Create sample transactions
+100.times do
+  user = User.offset(rand(User.count)).first
+  stock_symbols = %w[AAPL MSFT GOOG]
+  transaction = Transaction.create!(
+    symbol: stock_symbols.sample,
+    transaction_type: rand(2),
+    price: rand(1.0..100.0).round(2),
+    quantity: rand(1..10),
+    currency: 'USD',
+    user_id: user.id,
+    created_at: rand(Date.new(2024, 1, 1)..Date.new(2024,7,31))
+  )
+end
