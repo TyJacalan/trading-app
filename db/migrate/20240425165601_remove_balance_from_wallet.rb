df8b0d478e38b1ac91021e6e260af5a7d2c71601
Ty Jac <muzuuu@protonmail.com>
2024-04-30 12:48:56 +0800

    refactor: remove unused migrations

--- db/migrate/20240425165601_remove_balance_from_wallet.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBalanceFromWallet < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :wallets, :balance, :decimal
-  end
-end
--- db/migrate/20240425165753_add_transaction_type_and_amount_to_wallets.rb
+++
@@ -1,6 +0,0 @@
-class AddTransactionTypeAndAmountToWallets < ActiveRecord::Migration[7.1]
-  def change
-    add_column :wallets, :transaction_type, :integer
-    add_column :wallets, :amount, :decimal
-  end
-end
