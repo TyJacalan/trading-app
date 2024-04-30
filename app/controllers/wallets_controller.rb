8bdc51a90ca130b0a24afc6231d691922f4e1eb0
Ty Jac <muzuuu@protonmail.com>
2024-04-30 13:19:49 +0800

    refactor: implement deposity and withdraw transaction logic and wallet balance display

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -2,43 +2,7 @@ class WalletsController < ApplicationController
   before_action :authorize_user!
 
   def index
-    @wallet = Wallet.new
-    @balance = current_user.balance
-  end
-
-  def create
-    @wallet = Wallet.new(wallet_params)
-
-    if @wallet.save
-      case @wallet.transaction_type.to_sym
-      when :deposit
-        result = Wallet.deposit(current_user, @wallet.amount)
-      when :withdraw
-        result = Wallet.withdraw(current_user, @wallet.amount)
-      end
-
-      if result[:status]
-        flash.now[:notice] = "#{params[:wallet][:transaction_type].capitalize} successful!"
-      else
-        flash.now[:alert] = result[:error] || "Transaction failed"
-      end
-    else
-      flash.now[:alert] = @wallet.errors.full_messages.join(", ")
-    end
-
-    respond_to do |format|
-      format.turbo_stream
-    end
-  rescue StandardError => e
-    error_message = e.message
-    respond_to do |format|
-      format.turbo_stream { flash.now[:alert] = "#{error_message}" }
-    end
-  end
-
-  private
-
-  def wallet_params
-    params.require(:wallet).permit(:amount, :transaction_type).merge(user_id: current_user.id)
+    @transaction = Transaction.new(transaction_type: params[:transaction_type])
+    @balance = current_user.wallet.balance
   end
 end
